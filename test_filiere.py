6c0f48bc47e0c0e70fd645d07ac4ab3f7b64c2e0
aymantijan <79990284+aymantijan@users.noreply.github.com>
2024-12-26 15:40:38 +0100

    Update test_filiere.py

--- test_filiere.py
+++ test_filiere.py
@@ -4,3 +4,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 
+def main():
+    # Titre de l'application
+    st.title("Test d'Aide au Choix de Filière")
+    st.subheader("Analysez vos compétences et votre savoir-être pour découvrir la filière idéale.")
+
+    # Introduction
+    st.write("""
+    Ce test est conçu pour vous guider dans le choix de votre filière en fonction de :
+    - Vos compétences techniques.
+    - Vos capacités cognitives.
+    - Votre savoir-être (soft skills).
+    - Vos préférences psychologiques.
+    Répondez aux questions suivantes pour obtenir une recommandation personnalisée.
+    """)
